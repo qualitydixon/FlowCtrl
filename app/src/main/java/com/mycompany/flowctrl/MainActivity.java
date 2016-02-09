dc9ab132bd27da527357fe33b90680342d55e4d9
Mike <qualitydixon@gmail.com>
2016-02-09 15:12:23 -0700

    - New icon - Refactored scanning intent - General code clean up

--- app/src/main/java/com/mycompany/flowctrl/MainActivity.java
+++ app/src/main/java/com/mycompany/flowctrl/MainActivity.java
@@ -27,7 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase db;
     public static ArrayList<String> values;
     public static ArrayAdapter<String> adapter;
-    public TextView t;
+    public TextView status;
+    public TextView currentOut;
+    public int out = 0;
     public Button sO;
     public Button sI;
     private static final boolean VERBOSE = true;
@@ -40,16 +42,19 @@ public class MainActivity extends AppCompatActivity {
         mDbHelper = new MySQLiteHelper(this);
         setContentView(R.layout.activity_main);
 
+        // Set action bar title
         setTitle("Flow Ctrl");
 
         // Inject view elements
-        t = (TextView)this.findViewById(R.id.textView);
+        status = (TextView)this.findViewById(R.id.status);
+        currentOut = (TextView)this.findViewById(R.id.currentOut);
         sO = (Button)this.findViewById(R.id.scanOut);
         sI = (Button)this.findViewById(R.id.scanIn);
 
         // Set font
         Typeface main_font = Typeface.createFromAsset(getAssets(),  "fonts/Montserrat-Regular.ttf");
-        t.setTypeface(main_font);
+        status.setTypeface(main_font);
+        currentOut.setTypeface(main_font);
         sO.setTypeface(main_font);
         sI.setTypeface(main_font);
     }
@@ -85,31 +90,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void scanIn(View view){
-        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-        //intent.putExtra("SCAN_MODE", "PRODUCT_MODE");//for Qr code, its "QR_CODE_MODE" instead of "PRODUCT_MODE"
-        intent.putExtra("SAVE_HISTORY", true);//this stops saving ur barcode in barcode scanner app's history
-        startActivityForResult(intent, 0);
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan(0);
     }
 
     public void scanOut(View view){
-        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-        //intent.putExtra("SCAN_MODE", "PRODUCT_MODE");//for Qr code, its "QR_CODE_MODE" instead of "PRODUCT_MODE"
-        intent.putExtra("SAVE_HISTORY", true);//this stops saving ur barcode in barcode scanner app's history
-        startActivityForResult(intent, 1);
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan(1);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (requestCode == 1) {
             db = mDbHelper.getWritableDatabase();
 
             if (resultCode == RESULT_OK) {
                 String contents = data.getStringExtra("SCAN_RESULT"); //this is the result
-                System.out.println(contents + " " + DateFormat.getDateTimeInstance().format(new Date()));
-                t.setTextColor(getResources().getColor(R.color.holo_green));
-                t.setText(R.string.success);
-                //adapter.notifyDataSetChanged();
+                System.out.println(requestCode + "   " + resultCode + "   " + DateFormat.getDateTimeInstance().format(new Date()));
+
+                // Display success message
+                status.setTextColor(getResources().getColor(R.color.holo_green));
+                status.setText(R.string.success);
+
+                // Increment count;
+                out++;
+
+                // Add contents to database
                 ContentValues bc = new ContentValues();
                 bc.put(MySQLiteHelper.COLUMN_BARCODE, contents);
                 long newRowId;
@@ -128,27 +134,101 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 String contents = data.getStringExtra("SCAN_RESULT"); //this is the result
                 System.out.println(contents + DateFormat.getDateTimeInstance().format(new Date()));
+
+                // Make query
                 String q = "SELECT * FROM " + MySQLiteHelper.TABLE_TITLE + " WHERE " + MySQLiteHelper.COLUMN_BARCODE + " = '" + contents + "'";
                 Cursor c = db.rawQuery(q, null);
                 boolean isEmpty = c.getCount() < 1;
                 if(isEmpty) {
-                    t.setTextColor(getResources().getColor(R.color.darkred));
-                    t.setText(R.string.not_found);
+
+                    // Display not found message
+                    status.setTextColor(getResources().getColor(R.color.darkred));
+                    status.setText(R.string.not_found);
                 }
                 else {
-                    t.setTextColor(getResources().getColor(R.color.holo_green));
-                    t.setText(R.string.found);
 
-                    // Delete entry
+                    // Display found message
+                    status.setTextColor(getResources().getColor(R.color.holo_green));
+                    status.setText(R.string.found);
+
+                    // Decrement count;
+
+                    // Delete entry from database
                     String table = "beaconTable";
                     String whereClause =MySQLiteHelper.COLUMN_BARCODE + " = ? ";
                     String[] whereArgs = new String[] { contents };
                     db.delete(MySQLiteHelper.TABLE_TITLE, whereClause, whereArgs);
                 }
+
+                // Close cursor
                 c.close();
             } else if (resultCode == RESULT_CANCELED) {
                 // Handle cancel
             }
         }
     }
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            db = mDbHelper.getWritableDatabase();
+
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT"); //this is the result
+                System.out.println(contents + " " + DateFormat.getDateTimeInstance().format(new Date()));
+
+                // Display success message
+                status.setTextColor(getResources().getColor(R.color.holo_green));
+                status.setText(R.string.success);
+
+                // Add contents to database
+                ContentValues bc = new ContentValues();
+                bc.put(MySQLiteHelper.COLUMN_BARCODE, contents);
+                long newRowId;
+                newRowId = db.insert(
+                        MySQLiteHelper.TABLE_TITLE,
+                        null,
+                        bc);
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            }
+        }
+
+        if (requestCode == 0) {
+            db = mDbHelper.getReadableDatabase();
+
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT"); //this is the result
+                System.out.println(contents + DateFormat.getDateTimeInstance().format(new Date()));
+
+                // Make query
+                String q = "SELECT * FROM " + MySQLiteHelper.TABLE_TITLE + " WHERE " + MySQLiteHelper.COLUMN_BARCODE + " = '" + contents + "'";
+                Cursor c = db.rawQuery(q, null);
+                boolean isEmpty = c.getCount() < 1;
+                if(isEmpty) {
+
+                    // Display not found message
+                    status.setTextColor(getResources().getColor(R.color.darkred));
+                    status.setText(R.string.not_found);
+                }
+                else {
+
+                    // Display found message
+                    status.setTextColor(getResources().getColor(R.color.holo_green));
+                    status.setText(R.string.found);
+
+                    // Delete entry from database
+                    String table = "beaconTable";
+                    String whereClause =MySQLiteHelper.COLUMN_BARCODE + " = ? ";
+                    String[] whereArgs = new String[] { contents };
+                    db.delete(MySQLiteHelper.TABLE_TITLE, whereClause, whereArgs);
+                }
+
+                // Close cursor
+                c.close();
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            }
+        }
+    }*/
 }
